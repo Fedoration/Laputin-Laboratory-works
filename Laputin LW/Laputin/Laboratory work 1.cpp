00e54b372cd19cb76f7caa003f02590c21f2cb15
Fedoration <Fedoration@users.noreply.github.com>
2020-09-26 16:13:46 +0300

    some changes in programm code added 2 methods (read/write to file) for CS struct; a bite of optimization
    added 2 methods (read/write to file) for CS struct;
    a bite of optimization

--- Laputin LW/Laputin/Laboratory work 1.cpp
+++ Laputin LW/Laputin/Laboratory work 1.cpp
@@ -18,29 +18,30 @@ struct CS
 {
 	int id;
 	string name;
-	int num_workshops;
-	int num_running_workshops;
+	int count_workshops;
+	int count_running_workshops;
 	int efficiency;
 };
 
 
-// CRUD for Pipe
+/* CRUD for Pipe */
 Pipe createPipe()
 {
 	Pipe p;
 	cout << "Type pipe length: ";
 	cin >> p.length;
-	cout << "Type pipe diametеr: ";
+	cout << "Type pipe diameter: ";
 	cin >> p.diameter;
 	p.is_broken = false;
 	return p;
 }
 
-void Pipe_output(Pipe pipe) {
-	cout << "Output information about the pipe " << pipe.id << endl;
-	cout << "Pipe length: " << pipe.length << endl;
-	cout << "Pipe diameter: " << pipe.diameter << endl;
-	cout << "Pipe status: " << pipe.is_broken << endl;
+void Pipe_output(const Pipe& pipe) {
+	cout << "Output information about the pipe " << pipe.id << "\n"
+	     << "Pipe length: " << pipe.length << "\n"
+	     << "Pipe diameter: " << pipe.diameter << "\n"
+	     << "Pipe status: " << pipe.is_broken
+		 << endl;
 }
 
 void change_Pipe_status(Pipe& pipe) {
@@ -48,9 +49,9 @@ void change_Pipe_status(Pipe& pipe) {
 	cout << "Статус трубы был изменён на " << pipe.is_broken << endl;
 }
 
-Pipe read_from_file() {
+Pipe pipe_read_from_file() {
 	ifstream fin;
-	fin.open("Info.txt", ios::in);
+	fin.open("pipe_info.txt", ios::in);
 	if (fin.is_open())
 	{
 		Pipe p;
@@ -58,30 +59,82 @@ Pipe read_from_file() {
 		fin.close();
 		return p;
 	}
+	else {
+		cout << "File pipe_info.txt wasnt open!" << endl;
+	}
 }
 
-void write_to_file(Pipe pipe) {
+void pipe_write_to_file(Pipe pipe)
+{
 	ofstream fout;
-	fout.open("Info.txt", ios::out);
-	fout << pipe.id << endl << pipe.length << endl << pipe.diameter << endl << pipe.is_broken << endl;
-	fout.close();
+	fout.open("pipe_info.txt", ios::out);
+	if (fout.is_open())
+	{
+		fout << pipe.id << endl << pipe.length << endl << pipe.diameter << endl << pipe.is_broken << endl;
+		fout.close();
+		cout << "\nWriting to the file was successful" << endl;
+	}
+	else {
+		cout << "File pipe_info.txt wasnt open!" << endl;
+	}
 }
 
 
-// CRUD for Compressor station
+/* CRUD for Compressor station */
 CS createCS()
 {
 	CS cs;
 	cout << "Type KS name: ";
 	cin >> cs.name;
 	cout << "Type number of workshops: ";
-	cin >> cs.num_workshops;
+	cin >> cs.count_workshops;
 	cout << "Type number of running workshops: ";
-	cin >> cs.num_running_workshops;
+	cin >> cs.count_running_workshops;
 	cout << "Type KS efficiency: ";
 	cin >> cs.efficiency;
 
 	return cs;
+}
+
+void CS_output(const CS& cs) {
+	cout << "Output information about the compressor station " << cs.id << "\n"
+		 << "CS name: " << cs.name << "\n"
+	     << "Count of workshops: " << cs.count_workshops << "\n"
+	     << "Count of running workshops: " << cs.count_running_workshops
+		 << endl;
+}
+
+CS cs_read_from_file()
+{
+	ifstream fin;
+	fin.open("cs_info.txt", ios::in);
+	if (fin.is_open())
+	{
+		CS cs;
+		fin >> cs.id >> cs.name >> cs.count_workshops
+			>> cs.count_running_workshops >> cs.efficiency;
+		fin.close();
+		return cs;
+	}
+	else {
+		cout << "File cs_info.txt wasnt open!" << endl;
+	}
+}
+
+void cs_write_to_file(CS cs)
+{
+	ofstream fout;
+	fout.open("cs_info.txt", ios::out);
+	if (fout.is_open())
+	{
+		fout << cs.id << cs.name << cs.count_workshops
+			 << cs.count_running_workshops << cs.efficiency;
+		fout.close();
+		cout << "\nWriting to the file was successful" << endl;
+	}
+	else {
+		cout << "File cs_info.txt wasnt open!" << endl;
+	}
 }
 
 
@@ -91,10 +144,8 @@ int main()
 
 	Pipe pipe1 = createPipe();
 	pipe1.id = id;
+	pipe_write_to_file(pipe1);
 
-	//write_to_file(pipe1);
-	Pipe pipe2 = read_from_file();
-	Pipe_output(pipe2);
 
 	return 0;	
 }
